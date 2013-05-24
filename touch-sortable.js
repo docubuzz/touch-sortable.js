e27b08ba74efd662f72035d42ebee77b272c8b27
Ashish Puliyel <ashish@puliyel.com>
2013-05-24 17:30:50 +0800

    Now jumps multiple places in a single move (moving blocks, rather than single elements and cascading them).

--- touch-sortable.js
+++ touch-sortable.js
@@ -1,109 +1,114 @@
 (function ($) {
+    "use strict";
     $.fn.sortable = function (options) {
         options = options || {};
+        var startEvent = ('ontouchstart' in document.documentElement) ? 'touchstart' : 'mousedown';
+
         return this.each(function () {
-
-            var start_event_name = ('ontouchstart' in document.documentElement) ? 'touchstart' : 'mousedown';
-
-            $(this).children()
-                .on(start_event_name, onStart)
+            var parent = $(this);
+            var els = parent.children()
+                .on(startEvent, onStart)
                 .css({
                     cursor: "move",
                     'user-select': 'none'
-            	})
+                })
                 .attr('unselectable', 'on')
                 .on('selectstart', false);
 
-            var el;
-            var parentTop, parentBtm;
-            var start_y;
-            var elDistance = $(this).children(':nth-child(2)').offset().top - $(this).children(':nth-child(1)').offset().top;
+            /* If only one element we do nothing */
+            if (els.length < 2) {
+                return;
+            }
+
+            /* assumes the distance between the first and second element is the same as 
+               the distance between each other element and the next element */
+            var elDistance = els.filter(':nth-child(2)').offset().top - els.filter(':first').offset().top;
+
+            /* Set with each onStart and updated on move if necessary */
+            var el, parentTop, parentBtm, positionAtStart, hasQueuedAni;
 
             function onStart(e) {
-
                 e = e.originalEvent;
                 el = $(e.touches ? e.touches[0].target : e.target);
-                parentTop = el.parent().position().top;
-                parentBtm = parentTop + el.parent().innerHeight() + el.height();
-                el.addClass('inMotion').css({
-                    position: 'relative',
-                    'z-index': 1
-                });
+                parentTop = parent.position().top;
+                parentBtm = parentTop + parent.innerHeight() + el.height();
+
+                els.css('position','relative');
+                el.addClass('inMotion').css( 'z-index', 1);
+                hasQueuedAni = false;
 
                 /* Bind respective events based on start trigger */
                 if (e.touches) {
-                    start_y = e.touches[0].pageY;
+                    positionAtStart = e.touches[0].pageY;
                     $('body').on('touchmove.sortable', onMove).on('touchend.sortable', onEnd).on('touchcancel.sortable', onEnd);
                 } else {
-                    start_y = e.pageY;
+                    positionAtStart = e.pageY;
                     $('body').on('mousemove.sortable', onMove).on('mouseup.sortable', onEnd).on('mouseleave.sortable', onEnd);
                 }
             }
 
             function onMove(e) {
-                var move;
-                
+                var positionDelta;
                 if (e) {
                     e = e.originalEvent;
+                    var positionNow = (e.touches) ? e.touches[0].pageY : e.pageY;
 
-                    var pageY = (e.touches) ? e.touches[0].pageY : e.pageY;
+                    /* Constrain dragging to limits of parent */
+                    positionNow = Math.min(Math.max(parentTop, positionNow), parentBtm);
 
-                    /* Limit dragging to constraints of parent */
-                    if (pageY <= parentTop) {
-                        pageY = parentTop;
-                    } else if (pageY >= parentBtm) {
-                        pageY = parentBtm;
-                    }
-                  
                     /* If the cursor is near document boundary, scroll the page */
-                    if (50 >= (pageY - $(window).scrollTop())) {
+                    if (50 >= (positionNow - $(window).scrollTop())) {
                         window.scrollBy(0, -5);
                     } else if (50 >= $(window).height() + $(window).scrollTop()) {
                         window.scrollBy(0, 5);
                     }
 
                     /* Move item  */
-                    move = pageY - start_y;
-                    el.css('top', move);
+                    positionDelta = positionNow - positionAtStart;
+                    el.css('top', positionDelta);
                 } else {
-                    move = el.css('top').split('px')[0];
+                    positionDelta = el.css('top').split('px')[0];
                 }
 
-                /* Distance remaining to move, as measured by number of swaps */
-                var mvUnits = Math.abs(Math.floor(move/elDistance));
-
+                /* Distance remaining to move, as a number of elDistances */
+                var mvUnits = Math.floor(Math.abs(positionDelta / elDistance));
+                var sel;
                 /* Re-order the list once item crosses over the neighboring elements */
-                if (move < -elDistance) {
+                if (positionDelta < -elDistance && el.prev().length) {
 
-                    if (el.prev().length) {
-                        el.siblings().css('position', 'relative');
-                        if (!el.prev().is(':animated')) {
-
-                            /* Animate and swap */
-                            el.prev().animate({'top': elDistance}, 300/Math.pow(2,mvUnits), function () {
-                                start_y = start_y - elDistance;
-                                $(this).insertAfter(el).css({'top': ''});
-                                el.css('top', '+=' + elDistance);
-                                onMove();
-                            });
-                        }
+                    if (!els.filter(':animated').length) {
+                        hasQueuedAni = true;
+                        
+                        /* Animate and swap */
+                        sel = el.prevAll().slice(0, mvUnits);
+                        sel.animate({
+                            'top': elDistance
+                        }, 150).promise().done(function () {
+                            positionAtStart = positionAtStart - elDistance * mvUnits;
+                            el.insertBefore(sel.last()).css('top', '+=' + (elDistance * mvUnits));
+                            sel.css('top','');
+                            onMove();
+                        });
                     }
-                } else if (move > elDistance) {
+                } else if (positionDelta > elDistance && el.next().length) {
 
-                    if (el.next().length) {
-                        el.siblings().css('position', 'relative');
-                        if (!el.next().is(':animated')) {
-
-                            /* Animate and swap */
-
-                            el.next().animate({'top': -elDistance}, 300/Math.pow(2,mvUnits), function () {
-                                start_y = start_y + elDistance;
-                                $(this).insertBefore(el).css({'top': ''});
-                                el.css('top', '-=' + elDistance);
-                                onMove();
-                            });
-                        }
+                    if (!els.filter(':animated').length) {
+                        hasQueuedAni = true;
+                        
+                        /* Animate and swap */
+                        sel = el.nextAll().slice(0, mvUnits);
+                        sel.animate({
+                            'top': -elDistance
+                        }, 150).promise().done(function () {
+                            positionAtStart = positionAtStart + elDistance * mvUnits;
+                            el.insertAfter(sel.last()).css('top', '-=' + (elDistance * mvUnits));
+                            sel.css('top','');
+                            onMove();
+                        });
                     }
+
+                } else {
+                    hasQueuedAni = false;
                 }
 
                 if (e) {
@@ -111,20 +116,29 @@
                 }
             }
 
-            function onEnd(e) {
+            function onEnd() {
                 $('body').off('.sortable');
-
+                
                 function complete() {
-                    el.animate({'top': "-=" + el.css('top')}, 150, function () {
-                        el.css('top', 'auto').removeClass('inMotion').css('z-index', '');
-                        if(options.onComplete){
+                    el.animate({
+                        'top': "-=" + el.css('top')
+                    }, 150, function () {
+                        el.css({'top':'', 'z-index':''}).removeClass('inMotion');
+                        els.css('position','');                        
+                        if (options.onComplete) {
                             options.onComplete(el.parent());
                         }
                     });
                 }
-
-                if (el.prev().is(':animated') || el.next().is(':animated')) {
-                    setTimeout(function () { onEnd() }, 50);
+                /* Ensures all animations are complete, and the item is in it's
+                final position before completing. The promise() waits for the 
+                current animation to complete. setTimeout(x, 1) allows any remaning 
+                animation to be queued up */
+                var aniEls = els.filter(':animated');
+                if (aniEls.length || hasQueuedAni) {
+                    setTimeout(function () {
+                        aniEls.promise().done(onEnd);
+                    }, 1);
                 } else {
                     complete();
                 }
